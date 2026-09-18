886059e1f8c5f80ea27795dfa4add5bf4b89c47f
Vaibhav <coder8809@gmail.com>
2026-09-18 22:08:00 +0530

    chaining arena

--- arena.c
+++ arena.c
@@ -4,32 +4,38 @@
 #include<assert.h>
 #include "arena.h"
 
-#define ARENA_SIZE 4*1024 + sizeof(ArenaChunk) // 4kb + 24bytes
+#define ARENA_SIZE (4*1024 + sizeof(ArenaChunk)) // 4kb + 24bytes
 #define WORD_LEN 8
 
-Arena *A = {NULL};
-static int cnt = 0;
-static global_alloc_cnt = 1;
+//Arena *A = {NULL};
+//static int cnt = 0;
+//static int global_alloc_cnt = 1;
 
-void* ArenaAlloc(){
+void ArenaAlloc(void *A){
+    Arena *new_A = (Arena*)A;
     ArenaChunk *ptr = (ArenaChunk*)mmap(NULL, ARENA_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
-    assert(ptr != MAP_FAILED, "memory allocation failed");
-    if(global_alloc_cnt == 1){
-        A->first = ptr;
-        A->current = A->first;
-        A->current->next = NULL;
-        A->currrent->capacity = ARENA_SIZE;
-        A->current->offset = sizeof(ArenaChunk);
-        memset(A->current->check_size, 0, sizeof(A->current->check_size));
-        global_alloc_cnt++;
+    assert(ptr != MAP_FAILED && "memory allocation failed");
+    ptr->next = NULL;
+    ptr->capacity = ARENA_SIZE;
+    ptr->offset = sizeof(ArenaChunk);
+    ptr->cnt = 0;
+    memset(ptr->check_size, 0, sizeof(ptr->check_size));
+    if(new_A->first == NULL){
+        new_A->first = ptr;
+        new_A->current = new_A->first;
+        // new_A->current->next = NULL;
+        // new_A->current->capacity = ARENA_SIZE;
+        // new_A->current->offset = sizeof(ArenaChunk);
+        // memset(new_A->current->check_size, 0, sizeof(new_A->current->check_size));
+        //global_alloc_cnt++;
     }
     else{
-        A->current->next = ptr;
-        A->current = ptr;
-        A->current->next = NULL;
-        A->current->capacity = ARENA_SIZE;
-        A->current->offset = sizeof(ArenaChunk);
-        memset(A->current->check_size, 0, sizeof(A->current->check_size));
+        new_A->current->next = ptr;
+        new_A->current = ptr;
+        // new_A->current->next = NULL;
+        // new_A->current->capacity = ARENA_SIZE;
+        // new_A->current->offset = sizeof(ArenaChunk);
+        // memset(new_A->current->check_size, 0, sizeof(new_A->current->check_size));
     }
     /*
     A->base = ptr;
@@ -40,10 +46,11 @@ void* ArenaAlloc(){
     }
     */
 
-    return (uint8_t*)A->current + sizeof(ArenaChunk);
+    //return (void*)((uint8_t*)A->current + sizeof(ArenaChunk));
+    return;
 }
 
-void ArenaRelease(void *ptr){
+void ArenaRelease(void *A){
     /*
     int free = munmap(ptr, A->capacity);
     assert(free != -1, "error occured in deleting memory");
@@ -54,54 +61,83 @@ void ArenaRelease(void *ptr){
     }
     cnt = 0;
     */
-    ArenaChunk *ptr = A->first;
+   Arena *new_A = (Arena*)A;
+    ArenaChunk *ptr = new_A->first;
     while(ptr!=NULL){
+        ArenaChunk *next = ptr->next;
         int free = munmap(ptr, ptr->capacity);
-        assert(free != -1, "error occured in deleting memory");
-        ptr = ptr->next;
+        assert(free != -1 && "error occured in deleting memory");
+        ptr = next;
     }
+    new_A->first = NULL;
+    new_A->current = NULL;
     return;
 }
 
-void* ArenaPush(size_t size){
+void* ArenaPush(void *A, size_t size){
+    Arena *new_A = (Arena*)A;
+    if(new_A->first == NULL){
+        ArenaAlloc(new_A);
+    }
     int cpu_alignment = WORD_LEN;
-    size_t aligned_offset = (A->current->offset+(cpu_alignment-1))&(~(cpu_alignment-1));
-    assert(aligned_offset + size <= A->current->capacity, "Out of memory. Program crashed");
-    void *ptr = (uint8_t*)A->current + aligned_offset;
+    size_t aligned_offset = (new_A->current->offset+(cpu_alignment-1))&(~(cpu_alignment-1));
+    //assert(aligned_offset + size <= A->current->capacity, "Out of memory. Program crashed");
+    if(aligned_offset + size > new_A->current->capacity){
+        ArenaAlloc(new_A);
+        aligned_offset = (new_A->current->offset+(cpu_alignment-1))&(~(cpu_alignment-1));
+    }
+    void *ptr = (uint8_t*)new_A->current + aligned_offset;
     int rem_num = (size%8)==0 ?0 : 1;
     size_t allocated_size = 8*(size/8 + rem_num);
     memset(ptr, 0, allocated_size);
-    A->current->offset = aligned_offset + allocated_size;
-    A->current->check_size[cnt++] = allocated_size;
+    new_A->current->offset = aligned_offset + allocated_size;
+    int chunk_cnt = new_A->current->cnt;
+    assert(chunk_cnt < MAX_OBJ && "maximum size limit exceeded");
+    new_A->current->check_size[chunk_cnt] = allocated_size;
+    new_A->current->cnt++;
     return ptr;
 }
 
-void ArenaPop(){
-    A->current->offset = A->current->offset - A->current->check_size[--cnt];
+void ArenaPop(void *A){
+    Arena *new_A = (Arena*)A;
+    assert(new_A->current && new_A->current->cnt > 0);
+    int idx = new_A->current->cnt - 1;
+    new_A->current->offset = new_A->current->offset - new_A->current->check_size[idx];
+    new_A->current->cnt--;
     return;
 }
 
-void ArenaPopTo(int pos){
-    for(int i=cnt-1; i>=pos-1; --i){
-        A->current->offset = A->current->offset - A->current->check_size[i];
+void ArenaPopTo(void *A, int pos){
+    Arena *new_A = (Arena*)A;
+    if(new_A->current && pos > 0){
+        int idx = new_A->current->cnt;
+        for(int i=idx-1; i>=pos-1; --i){
+        new_A->current->offset = new_A->current->offset - new_A->current->check_size[i];
+        new_A->current->cnt--;
+      }
     }
-    cnt=cnt-pos-1;
+    //cnt=cnt-pos-1;
     return;
 }
 
-void ArenaClear(){
+void ArenaClear(void *A){
     //A->offset = 0;
-    ArenaChunk *ptr = A->first;
+    Arena *new_A = (Arena*)A;
+    ArenaChunk *ptr = new_A->first;
     while(ptr != NULL){
+        ArenaChunk *next = ptr->next;
         ptr->offset = sizeof(ArenaChunk);
-        ptr = ptr->next;
+        ptr->cnt = 0;
+        ptr = next;
     }
+    new_A->current = new_A->first;
     return;
 }
-
+/*
 int main(int argc, char *argv[]){
     printf("Hello World\n");
-    void *ptr = ArenaAlloc(); 
+    //void *ptr = ArenaAlloc(); 
     printf("%p\n", ptr);
     return 0;
 }
+*/
--- arena.h
+++ arena.h
@@ -2,22 +2,23 @@
 #define _ARENA_H_
 #define MAX_OBJ 100
 
-typedef Arena{
-    void *first; // first block of memory
-    void *current; // second block of memory
-}
-
 typedef struct ArenaChunk{
-    void* next; // pointing to the base of arena
+    struct ArenaChunk* next; // pointing to the base of arena
     size_t offset; 
     size_t capacity;
+    int cnt;
     size_t check_size[MAX_OBJ];
+}ArenaChunk;
+
+typedef struct Arena{
+    struct ArenaChunk *first; // first block of memory
+    struct ArenaChunk *current; // second block of memory
 }Arena;
 
-void* ArenaAlloc(); // for creating arena
-void ArenaRelease(void *ptr); // to free arena
-void* ArenaPush(size_t s); // for creating a buffer in arena of size s (in bytes) from the top
-void ArenaPop(); // removing the topmost/last object from arena 
-void ArenaPopTo(int pos); // removing the objects from arena after position pos from top
-void ArenaClear(); // clearing arena
+void ArenaAlloc(void *A); // for creating arena
+void ArenaRelease(void *A); // to free arena
+void* ArenaPush(void *A, size_t s); // for creating a buffer in arena of size s (in bytes) from the top
+void ArenaPop(void *A); // removing the topmost/last object from arena 
+void ArenaPopTo(void *A, int pos); // removing the objects from arena after position pos from top
+void ArenaClear(void *A); // clearing arena
 #endif
